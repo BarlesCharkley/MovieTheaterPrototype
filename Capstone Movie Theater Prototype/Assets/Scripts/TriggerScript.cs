165d0208868bdb654849c4cf2cb03b09f19550eb
3voker <Drechristophe@gmail.com>
2017-09-20 17:37:40 -0500

    Added Details
    Added Trigger Script, Rain Trigger, details to Marquee panels.

---
+++ Capstone Movie Theater Prototype/Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour {
+
+    // Use this for initialization
+    [SerializeField]
+    AudioSource audioSource;
+    [SerializeField]
+    GameObject particleSystem;
+
+    //GameObject game
+    //   public GameObject;
+    void Start()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            particleSystem.SetActive(false);
+            audioSource.Stop();
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            particleSystem.SetActive(true);
+            audioSource.Play();
+
+        }
+    }
+}
